503b3ff618b091eca95ab8ab4e032a31df7aa4ff
NaniGudivaka <nanigudivaka0804@gmail.com>
2026-04-10 15:45:31 +0530

    Currently working on about component

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import '../styles/about.css';
 
 const About = () => {
   return (
      <section id="about" className="about">
-      <h2>About Me</h2>
+      <h2 className='header-section'>About Me</h2>
       <p>
         I am a frontend developer skilled in HTML, CSS, JavaScript and React.
         I build clean and responsive web applications.
